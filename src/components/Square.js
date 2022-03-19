6a76ddad91addd8bcbfc447de1e2a0ca57ee6765
PatrickYu23 <86265685+PatrickYu7@users.noreply.github.com>
2022-03-18 23:46:15 -0700

    finished

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import './styles/Square.css';
 
-function Square() {
-    return (
-        <button className="square">
-            {/* TODO */}
-        </button>
-    )
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.click()}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
